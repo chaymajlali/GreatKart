dd00abb9d6ed52869e6516ae8f101c6435038f47
aouinti mootez <mootezaouinti1@gmail.com>
2025-11-15 11:57:02 +0100

    dashboard page

--- account/views.py
+++ account/views.py
@@ -7,6 +7,7 @@ from carts.models import Cart, CartItem
 
 from .forms import RegistrationForm
 from .models import Account
+from orders.models import Order
 from django.contrib.auth.decorators import login_required
 from carts.views import _cart_id 
 #verification email
@@ -150,7 +151,12 @@ def activate(request, uidb64, token):
 
 @login_required(login_url='login')
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered=True)
+    orders_count = orders.count()
+    context = {
+        'orders_count': orders_count,
+    }
+    return render(request, 'accounts/dashboard.html', context)
 def forgotPassword(request):
     if request.method == 'POST':
         email = request.POST['email']
